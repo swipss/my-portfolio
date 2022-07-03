ffdcba345ceadd461301647b5f7ded6bc617eb4d
swipss <vassiljevsten1@gmail.com>
2022-07-03 11:22:23 +0300

    change navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,6 @@ export const Navbar = () => {
             />
           </a>
         </div>
-
         <img
           src={require("../images/moon.png")}
           className="h-8 cursor-pointer"
